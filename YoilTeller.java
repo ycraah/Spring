8d7363e1932ca57c00ca9b0b47bcebef02d28965
ycraah <102202147+ycraah@users.noreply.github.com>
2024-07-17 15:00:41 +0900

    Update YoilTeller.java
    MVC에 따라 입력, 작업, 출력 분리
    유효성 검사 추가(후에 보완 필요)

--- YoilTeller.java
+++ YoilTeller.java
@@ -10,40 +10,42 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 class YoilTeller{
 	@RequestMapping("/date")
-	public void main(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		//입력
-		String yyyy = request.getParameter("year");
-		String MM = request.getParameter("month");
-		String dd = request.getParameter("day");
+	public String main(int year, int month, int day, Model model) throws IOException {
+		//유효성 검사
+		if(!isValid(year,month,day)) {
+			return "error";
+		}
 		
-		int year = Integer.parseInt(yyyy);
-		int month = Integer.parseInt(MM);
-		int day = Integer.parseInt(dd);
-			
-		//작업
+		String yoil = getYoil(year, month, day);
+		
+		model.addAttribute("year", year);
+		model.addAttribute("month", month);
+		model.addAttribute("day", day);
+		model.addAttribute("yoil", yoil);
+		
+		return "yoil";
+	}
+
+	private String getYoil(int year, int month, int day) {
 		Calendar cal = Calendar.getInstance();
 		cal.set(year, month-1, day);
 
-		//출력
 		int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
 		String[] yoilArr = {"", "일", "월", "화", "수", "목", "금", "토"};
 		String yoil = yoilArr[dayOfWeek];
-		
-		response.setContentType("text/html");
-		response.setCharacterEncoding("utf-8");
-		PrintWriter out = response.getWriter();
-		out.println("<html>");
-		out.println("<head>");
-		out.println("</head>");
-		out.println("<body>");
-		out.println("<p>" + year + "년 " + month + "월 " + day +"일은 " + yoil + "요일입니다." + "</p>");
-		out.println("</body>");
-		out.println("</html>");
-		
+		return yoil;
+	}
+
+	private boolean isValid(int year, int month, int day) {
+		if(0 < year && 0<=month && month<12 && 1<=day && day<=31) {
+			return true;
+		}
+		return false;
 	}
 }
