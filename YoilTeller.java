1a99e6d92c86aee1911ac7e0ccd63baa8a863770
ycraah <102202147+ycraah@users.noreply.github.com>
2024-07-17 23:50:23 +0900

    yoil 도출을 위한 배열을 string에서 char로 변환

--- YoilTeller.java
+++ YoilTeller.java
@@ -1,51 +1,43 @@
 package com.firstSpring.app;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-class YoilTeller{
+public class YoilTeller {
 	@RequestMapping("/yoil")
-	public String main(int year, int month, int day, Model model) throws IOException {
+	public String main(int year, int month, int day, Model model){
 		//유효성 검사
-		if(!isValid(year,month,day)) {
+		if(!isValid(year, month, day)) {
+			//오류페이지
 			return "error";
 		}
 		
-		String yoil = getYoil(year, month, day);
+		//작업
+		char yoil = getYoil(year, month, day);
 		
+		//model에 전달
 		model.addAttribute("year", year);
 		model.addAttribute("month", month);
 		model.addAttribute("day", day);
 		model.addAttribute("yoil", yoil);
 		
+		//정상페이지
 		return "yoil";
 	}
 
-	private String getYoil(int year, int month, int day) {
+	private char getYoil(int year, int month, int day) {
 		Calendar cal = Calendar.getInstance();
 		cal.set(year, month-1, day);
-
-		int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
-		String[] yoilArr = {"", "일", "월", "화", "수", "목", "금", "토"};
-		String yoil = yoilArr[dayOfWeek];
-		return yoil;
+		int yoilNum = cal.get(Calendar.DAY_OF_WEEK);
+		char[] yoilArr = {' ', '일', '월', '화', '수', '목', '금', '토'};
+		return yoilArr[yoilNum];
 	}
 
 	private boolean isValid(int year, int month, int day) {
-		if(0 < year && 0<=month && month<12 && 1<=day && day<=31) {
-			return true;
-		}
-		return false;
+		return true;
 	}
 }
